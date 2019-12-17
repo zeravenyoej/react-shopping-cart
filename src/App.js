3cfda15e3d1a6bf8fbfc7f2e48c4e82d8c1ba72b
Joey Nevarez <josephnevarez12@gmail.com>
2019-12-17 12:27:21 -0800

    refractored nav and shoppingcart components, made MVP

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -21,15 +22,16 @@ function App() {
 	};
 
 	return (
-		//do I need to add cart state to shopping cart here?
 		<ProductContext.Provider value={{ products, addItem }}>
-			<div className="App">
-				<Navigation cart={cart} />
-
-				<Route exact path="/" component={Products}/>
-
-				<Route path="/cart" component={ShoppingCart}/>
-			</div>
+			<CartContext.Provider value={{cart}}>
+				<div className="App">
+					<Navigation cart={cart} />
+					
+					<Route exact path="/" component={Products}/>
+					
+					<Route path="/cart" component={ShoppingCart}/>
+				</div>
+			</CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
